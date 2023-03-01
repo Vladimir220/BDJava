d749b3a7ae4c8620f396f40371e44a1b7ece6e2e
Vladimir220 <62606736+Vladimir220@users.noreply.github.com>
2023-03-02 01:14:27 +0300

    Update Program.java

--- Lab1/src/bdjava/lab1/Program.java
+++ Lab1/src/bdjava/lab1/Program.java
@@ -1,10 +1,22 @@
 package bdjava.lab1;
+import static java.lang.System.*;
 
 public class Program {
-
+	// Создать приложение для ввода пароля из командной строки и сравнения его со строкой-образцом
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		final String sample = "qwerty";
+		
+		try {
+			if(args.length < 1)
+				throw new Exception("No input");
+			
+			if(args[0].equals(sample))
+				out.println("Passwords match");
+			else
+				out.println("Password mismatch");
+		}
+		catch(Exception e) {
+			out.println(e.getMessage());
+		}
 	}
-
 }

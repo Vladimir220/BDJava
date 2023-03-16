4186fa31f1f8997bb6b190a79e9a809da54e9d89
Vladimir220 <62606736+Vladimir220@users.noreply.github.com>
2023-03-16 16:06:16 +0300

    ЛР3.1.2
    Осталось сложение соседей

--- src/bdjava/lab3/var1/Program.java
+++ src/bdjava/lab3/var1/Program.java
@@ -15,13 +15,22 @@ import java.util.Scanner;
  */
 public class Program {
 	public static void main(String[] args) {
-		out.println("Часть 1:");
-		ContinuedFraction frac = new ContinuedFraction((short) 5,3.6f);
-		out.println("Результат: " + frac.calculate(true));
-		out.println(frac.getFrac());
+		/*out.println("Часть 1:");
+		ContinuedFraction frac1 = new ContinuedFraction((short) 5,3.6f);
+		out.println("Результат: " + frac1.calculate(true));
+		out.println(frac1.getFrac());
 		out.println();
-		out.println("Часть 2:");
-
+		out.println("Часть 2:");*/
+		Fraction frac2 = new Fraction(232,8);
+		Fraction frac3 = new Fraction(32,7);
+		Fraction sum = Fraction.addition(frac2, frac3);
+		Fraction sub = Fraction.subtraction(frac2, frac3);
+		Fraction mul = Fraction.multiplication(frac2, frac3);
+		Fraction div = Fraction.division(frac2, frac3);
+		out.println(frac2 + " + " + frac3 + " = " + sum);
+		out.println(frac2 + " - " + frac3 + " = " + sub);
+		out.println(frac2 + " * " + frac3 + " = " + mul);
+		out.println(frac2 + " / " + frac3 + " = " + div);
 	}
 }
 
@@ -72,5 +81,79 @@ class ContinuedFraction {
 }
 
 class Fraction{
-	
+	private int m;
+	private int n;
+
+	Fraction(int m){
+		this.m = m;
+		this.n = 1;
+	}
+	Fraction(int m, int n){
+		this.m = m;
+		this.n = n;
+	}
+	Fraction(){
+		this(1);
+	}
+	public static Fraction addition(Fraction first, Fraction second){
+		Fraction bufF = new Fraction();
+		int bufI1, bufI2;
+		Fraction.fracReduction(first);
+		Fraction.fracReduction(second);
+		bufF.n = first.n * second.n;
+		bufI1 = first.m * second.n;
+		bufI2 = second.m * first.n;
+		bufF.m = bufI1 + bufI2;
+		Fraction.fracReduction(bufF);
+		return bufF;
+	}
+	public static Fraction subtraction(Fraction first, Fraction second){
+		Fraction bufF = new Fraction();
+		int bufI1, bufI2;
+		Fraction.fracReduction(first);
+		Fraction.fracReduction(second);
+		bufF.n = first.n * second.n;
+		bufI1 = first.m * second.n;
+		bufI2 = second.m * first.n;
+		bufF.m = bufI1 - bufI2;
+		Fraction.fracReduction(bufF);
+		return bufF;
+	}
+	public static Fraction multiplication(Fraction first, Fraction second){
+		Fraction bufF = new Fraction();
+		Fraction.fracReduction(first);
+		Fraction.fracReduction(second);
+		bufF.n = first.n * second.n;
+		bufF.m = first.m * second.m;
+		Fraction.fracReduction(bufF);
+		return bufF;
+	}
+	public static Fraction division(Fraction first, Fraction second){
+		Fraction bufF = new Fraction(), divisor = new Fraction(second.n, second.m);
+		Fraction.fracReduction(first);
+		Fraction.fracReduction(second);
+		bufF.n = first.n * divisor.n;
+		bufF.m = first.m * divisor.m;
+		Fraction.fracReduction(bufF);
+		return bufF;
+	}
+	public static double neighborAddition(Fraction first, Fraction second){
+		return 2.2;
+	}
+	private static void fracReduction(Fraction frac){
+		int first = frac.m;
+		int second = frac.n;
+		int nod;
+		while (first != second)
+			if(first > second)
+				first -= second;
+			else
+				second -= first;
+		nod = first;
+		frac.m /= nod;
+		frac.n /= nod;
+	}
+	public String toString(){
+		return this.m + "/" + this.n;
+	}
 }

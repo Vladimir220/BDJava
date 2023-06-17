5f10ba513956e46d15d42d0ea70d20958291aa52
Vladimir220 <62606736+Vladimir220@users.noreply.github.com>
2023-06-17 04:05:49 +0300

    LR6.1

---
+++ src/lab6/Part1.java
@@ -0,0 +1,30 @@
+package lab6;
+
+import java.util.ArrayList;
+import static java.lang.System.out;
+
+public class Part1 {
+    public static void main(String[] args) {
+            ArrayList<Integer> list = new ArrayList<>();
+            list.add(3);
+            list.add(-2);
+            list.add(4);
+            list.add(-1);
+            list.add(-5);
+            list.add(0);
+            list.add(2);
+
+            int size = list.size();
+            int j = 0;
+            for (int i = 0; i < size; i++) {
+                int element = list.get(j);
+                if (element < 0) {
+                    list.remove(j);
+                    list.add(element);
+                } else {
+                    j++;
+                }
+            }
+            out.println(list);
+    }
+}

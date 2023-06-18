27241708335d268787f70c24b734146727d92baa
Vladimir220 <62606736+Vladimir220@users.noreply.github.com>
2023-06-18 21:38:03 +0300

    LR8_2

---
+++ src/lab8/Part2.java
@@ -0,0 +1,42 @@
+package lab8;
+
+//2.	Реализовать многопоточное приложение “Робот”. Надо написать
+// робота, который умеет ходить. За движение каждой его ноги отвечает отдельный
+// поток. Шаг выражается в выводе в консоль LEFT или RIGHT.
+public class Part2 {
+    public static void main(String[] args) {
+        Thread leftLegT = new Thread(new LeftLeg());
+        Thread rightLegT = new Thread(new RightLeg());
+
+        leftLegT.start();
+        rightLegT.start();
+    }
+}
+
+class LeftLeg implements Runnable {
+    @Override
+    public void run() {
+        while (true) {
+            System.out.println("LEFT");
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+}
+
+class RightLeg implements Runnable {
+    @Override
+    public void run() {
+        while (true) {
+            System.out.println("RIGHT");
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+}
